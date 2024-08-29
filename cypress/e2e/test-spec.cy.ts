182c9f646cfe83b113e196e2c8f2032ac3922fa3
Ashwin Jain <1ashwinjain@gmail.com>
2024-08-29 17:09:06 -0500

    added basic setup for detox - build not working

--- cypress/e2e/test-spec.cy.ts
+++
@@ -1,9 +0,0 @@
-describe("template spec", () => {
-  it("passes", () => {
-    cy.visit("https://example.cypress.io");
-    cy.fixture("example.json").then((example) => {
-      expect(example.email).to.be.eq("1ashwinjain@gmail.com"); // checking equality
-      expect(example.email).to.not.be.eq("ashwinjain1@gmail.com"); // ensuring not inequality
-    });
-  });
-});
